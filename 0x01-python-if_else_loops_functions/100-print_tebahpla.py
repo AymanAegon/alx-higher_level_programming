b45ff200eed4edec4e47a7038f75a330584391ae
AymanAegon <ayo12ammar24@gmail.com>
2023-06-07 09:58:41 -0700

    14. Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-i = 0
 for alpha in reversed(range(ord('a'), ord('z')+1)):
-    if i % 2 == 1:
+    if alpha % 2 == 1:
         print(chr(alpha).upper(), end='')
     else:
         print(chr(alpha), end='')
-    i += 1
