4f456426754186cec1669202434d043f469351b3
AymanAegon <ayo12ammar24@gmail.com>
2023-06-10 07:02:25 -0700

    13. Linked list palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+int is_palindrome(listint_t **head)
+{
+	listint_t *temp;
+	int len = 0, i = 0, *arr;
+
+	temp = *head;
+	
+	while (temp)
+	{
+		len++;
+		temp = temp->next;
+	}
+	temp = *head;
+	arr = (int *)malloc(sizeof(int) * len / 2);
+	for (i = 0; i < len / 2; i++)
+	{
+		arr[i] = temp->n;
+		temp = temp->next;
+	}
+	for (i = len / 2 - 1; i >= 0; i--)
+	{
+		if (arr[i] != temp->n)
+			return 0;
+		temp = temp->next;
+	}
+	free(arr);
+	return 1;
+}
