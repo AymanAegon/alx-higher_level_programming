a50440cf46bd95cd9acab3576de8ef8432afcad0
AymanAegon <ayo12ammar24@gmail.com>
2023-06-14 06:23:12 -0700

    10. Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    if len(a_dictionary) <= 0:
+        return None
+    a = list(a_dictionary.keys())
+    max = a[0]
+    for n in a:
+        if a_dictionary[n] > a_dictionary[max]:
+            max = n
+    return max
