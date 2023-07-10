8cbc44a0317e83e2110876115d7827dd78a84856
AymanAegon <ayo12ammar24@gmail.com>
2023-07-10 15:20:54 -0700

    11. Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Square Module
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    inherits from Rectangle
+    """
+    def __init__(self, size):
+        """
+        initializes a square
+
+        Args:
+            size (int): size of the square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        calculate the area
+
+        Returns:
+            area result
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        return following rectangle description:
+        [Square] <width>/<height>
+
+        Returns:
+            [Square] <width>/<height>
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
