16917074b365d51e9c4a4414c9dc582a88703cf9
AymanAegon <ayo12ammar24@gmail.com>
2023-07-10 15:38:31 -0700

    12. My integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -11,16 +11,24 @@ class MyInt(int):
     def __eq__(self, x):
         """
         invert to !=
+
         Args:
             x (int): other int
+
+        Return:
+            self != x
         """
         return super().__ne__(x)
 
     def __ne__(self, x):
         """
         invert to ==
+
         Args:
             x (int): other int
+
+        Return:
+            self == x
         """
         return super().__eq__(x)
 
