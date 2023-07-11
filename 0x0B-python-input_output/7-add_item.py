08f82b46ba4c435a435d19b5842f7166f29fa3b8
AymanAegon <ayo12ammar24@gmail.com>
2023-07-11 13:18:02 -0700

    7. Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
-import sys, json
 
 
 filename = "add_item.json"
