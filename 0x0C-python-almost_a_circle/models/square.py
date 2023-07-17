c9bb9b82fb94353c78545a3ac97faf237b87823a
Ayman <65777259+AymanAegon@users.noreply.github.com>
2023-07-17 17:39:28 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,6 +17,7 @@ class Square(Rectangle):
         Creates a Square
         """
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         """
@@ -27,13 +28,15 @@ class Square(Rectangle):
         y = self.y
         size = self.width
         return "[Square] ({}) {}/{} - {}".format(id, x, y, size)
-
+    
+    @property
     def size(self):
         """
         gets the size
         """
         return self.width
-
+    
+    @size.setter
     def size(self, value):
         """
         sets the size
@@ -47,7 +50,7 @@ class Square(Rectangle):
             if len(args) >= 1:
                 self.id = args[0]
             if len(args) >= 2:
-                self.size(args[1])
+                self.size = args[1]
             if len(args) >= 3:
                 self.x = args[2]
             if len(args) >= 4:
@@ -56,12 +59,12 @@ class Square(Rectangle):
         if "id" in kwargs:
             self.id = kwargs["id"]
         if "size" in kwargs:
-            self.size(kwargs["size"])
+            self.size = kwargs["size"]
         if "x" in kwargs:
             self.x = kwargs["x"]
         if "y" in kwargs:
             self.y = kwargs["y"]
-
+    
     def to_dictionary(self):
         """
         returns the dictionary representation of a Square
@@ -69,7 +72,7 @@ class Square(Rectangle):
         d = {
             'id': self.id,
             'x': self.x,
-            'size': self.size(),
+            'size': self.size,
             'y': self.y
         }
         return d
