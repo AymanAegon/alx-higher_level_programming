5d88a31096bd32a173966366349020e3ab64d1a5
AymanAegon <ayo12ammar24@gmail.com>
2023-08-16 09:55:01 -0700

    13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+
+SELECT tg.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_genres tg INNER JOIN tv_show_genres tsg
+ON tg.id = tsg.genre_id
+GROUP BY tsg.genre_id
+ORDER BY number_of_shows DESC;
