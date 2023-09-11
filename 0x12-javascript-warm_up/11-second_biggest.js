3fe16a5dfa1917b1dadce2b4ce7ba60f67bd52bc
AymanAegon <ayo12ammar24@gmail.com>
2023-09-11 07:25:07 -0700

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
 const arr = process.argv;
-let x = arr[2]; let y;
+let x, y;
 if (arr.length <= 3) {
   console.log(0);
 } else {
+  for (let i = 2; i < arr.length; i++) {
+    arr[i] = parseInt(arr[i]);
+  }
+  x = arr[2];
+  y = arr[3];
   for (let i = 3; i < arr.length; i++) {
     if (arr[i] > x) {
       y = x;
