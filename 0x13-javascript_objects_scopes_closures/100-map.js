d04a53572d7e74021660e7ce6a2b7fc2bfe9fc92
AymanAegon <ayo12ammar24@gmail.com>
2023-09-12 07:18:38 -0700

    11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-const { list } = require("./100-data");
+const { list } = require('./100-data');
+let i = 0;
 
 console.log(list);
-const map1 = list.map((x) => x * (x - 1));
+const map1 = list.map((x) => x * i++);
 console.log(map1);
