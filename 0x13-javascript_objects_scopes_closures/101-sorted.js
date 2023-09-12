95f93cd1be0a8f49ff0bf97d440add25594f39bc
AymanAegon <ayo12ammar24@gmail.com>
2023-09-12 07:54:00 -0700

    12

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,10 +2,9 @@ const { dict } = require('./101-data');
 const res = {};
 
 for (const key in dict) {
-  if (res[dict[key]]) {
-    res[dict[key]].push(key);
-  } else {
-    res[dict[key]] = [key];
+  if (res[dict[key]] === undefined) {
+    res[dict[key]] = [];
   }
+  res[dict[key]].push(key);
 }
 console.log(res);
