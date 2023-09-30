096f37b25bd54600c48df1e64b780fb0f567c054
AymanAegon <ayo12ammar24@gmail.com>
2023-09-30 06:45:06 -0700

    2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,6 +8,7 @@ import sys
 if __name__ == "__main__":
     post_dict = {"email": sys.argv[2]}
     url_encoded_data = urllib.parse.urlencode(post_dict)
-    post_data = url_encoded_data.encode("utf-8")
-    with urllib.request.urlopen(sys.argv[1], data=post_data) as response:
-        print(f"Your email is: {response.getheader('email')}")
+    post_data = url_encoded_data.encode("ascii")
+    req = urllib.request.Request(sys.argv[1], data=post_data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
