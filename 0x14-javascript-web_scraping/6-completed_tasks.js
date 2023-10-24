f685eab3be8eb641cda3a45251ca3b28a8779fd7
ayman <ayo12ammar24@gmail.com>
2023-10-24 07:41:12 -0700

    6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response) => {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const d = {};
+    const data = JSON.parse(response.body);
+    data.forEach(todo => {
+      if (d[todo.userId] === undefined) {
+        d[todo.userId] = 0;
+      } else if (todo.completed === true) {
+        d[todo.userId] += 1;
+      }
+    });
+    console.log(d);
+  } else {
+    console.log('Erorr Code:' + response.statusCode);
+  }
+});
