174d232d47336a8b626bbdb6c7c86297559c58d7
ayman <ayo12ammar24@gmail.com>
2023-11-01 07:02:38 -0700

    9

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+  $.get(url, function (data, textStatus, jqXHR) {
+    console.log(data);
+    $('DIV#hello').text(data.hello);
+  });
+});
